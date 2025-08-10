053d6d47b03eabe0f885e7abc92f2c6ff4091f48
Tanzil <tanzilibrahim5@gmail.com>
2025-08-10 07:33:55 +0530

    update

---
+++ clas.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class clas {
+    class A{
+       public int sum(int x,int y){
+             return x+y;
+
+        }
+    }
+    public static void main(String[] args) {
+        clas outer = new clas();
+        A z = outer.new A();
+        Scanner sc=new Scanner(System.in);
+        int j=sc.nextInt();
+        System.out.println("enter first number:");
+        int k=sc.nextInt();
+        System.out.println("enter second number:");
+    
+        
+
+
+        
+    }
+    
+}
