fcf94b1a0fe6215b72d515e07866550ff64950f9
Tanzil <tanzilibrahim5@gmail.com>
2025-08-14 19:30:15 +0530

    update

---
+++ Arra2.java
@@ -0,0 +1,8 @@
+public class Arra2 {
+    public static void main(String[] args) {
+        int[] ros;
+        ros = new int[5];
+        // for an integer array by default it's just going to have all the zero elements.
+        System.out.println(ros[1]); // output:0
+    }
+}
