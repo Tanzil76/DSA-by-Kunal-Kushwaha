c0517c57dd1065479d9315645c42ed211a188fd4
Tanzil <tanzilibrahim5@gmail.com>
2025-08-16 11:52:52 +0530

    update

--- input2.java
+++ input2.java
@@ -6,13 +6,7 @@ public class input2 {
         Scanner in = new Scanner(System.in);
         // arrays of primitives
         int[] arr = new int[5];
-    //    arr[0] = 23;
-    //    arr[1] = 45;
-    //    arr[2] = 233;
-    //    arr[3] = 543;
-    //    arr[4] = 3;
-        // [23, 45, 233, 543, 3]
-    //    System.out.println(arr[3]);
+    
 
         // input using for loops
         for (int i = 0; i < arr.length; i++) {
