4fda4aeb1e5101358b57bb342caba78834218f16
Tanzil <tanzilibrahim5@gmail.com>
2025-08-16 16:41:15 +0530

    update

---
+++ input3.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class input3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // arrays of objects
+        String[] str = new String[4];
+        for ( int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+        }
+        System.out.println(Arrays.toString(str));
+    }
+}
