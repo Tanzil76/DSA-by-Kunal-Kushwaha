16cefdadc3e4dbe20fbbc4184fedecbc6b9593e7
Tanzil <tanzilibrahim5@gmail.com>
2025-08-17 15:26:16 +0530

    update

--- TwoDarra.java
+++ TwoDarra.java
@@ -14,18 +14,18 @@ public class TwoDarra {
             }
         }
 
-    /*  output
-        for(int row = 0; row < arr.length; row++){
-            for(int col = 0; col < arr[row].length; col++){
-                System.out.print(arr[row][col] + " ");
-            }
-                System.out.println();    output : 1 2 3
-                                                  4 5 6
-                                                  7 8 9    
+        // output
+    //    for(int row = 0; row < arr.length; row++){
+    //        for(int col = 0; col < arr[row].length; col++){
+    //            System.out.print(arr[row][col] + " ");
+    //        }
+    //            System.out.println();    output : 1 2 3
+    //                                              4 5 6
+    //                                              7 8 9    
 
-            System.out.println(Arrays.toString(arr[row]));   output :   [1, 2, 3]
-                                                                        [4, 5, 6]
-                                                                        [7, 8, 9]         */
+    //        System.out.println(Arrays.toString(arr[row]));  // output :   [1, 2, 3]
+                                                            //              [4, 5, 6]
+                                                            //              [7, 8, 9]
         // enhanced for loop
         for(int[] a : arr) {
             System.out.println(Arrays.toString(a));                                                      
