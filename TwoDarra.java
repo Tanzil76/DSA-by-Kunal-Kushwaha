8c3cc9c87b5e725e2b659cd68254e7a32c7306cd
Tanzil <tanzilibrahim5@gmail.com>
2025-08-17 15:18:23 +0530

    update

---
+++ TwoDarra.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class TwoDarra {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int[][] arr = new int[3][3];
+
+        // input
+        for(int row = 0; row < arr.length; row++){
+            // for each col in every row
+            for(int col = 0; col < arr[row].length; col++){
+                arr[row][col] = in.nextInt();
+            }
+        }
+
+    /*  output
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col < arr[row].length; col++){
+                System.out.print(arr[row][col] + " ");
+            }
+                System.out.println();    output : 1 2 3
+                                                  4 5 6
+                                                  7 8 9    
+
+            System.out.println(Arrays.toString(arr[row]));   output :   [1, 2, 3]
+                                                                        [4, 5, 6]
+                                                                        [7, 8, 9]         */
+        // enhanced for loop
+        for(int[] a : arr) {
+            System.out.println(Arrays.toString(a));                                                      
+        }       
+    }
+}
