24232443894bd2eb620cc09f48dcc48c1024873c
Tanzil <tanzilibrahim5@gmail.com>
2025-08-19 08:04:22 +0530

    update

--- Swap.java
+++ Swap.java
@@ -9,6 +9,7 @@ public class Swap {
         swap(arr, 1, 3);
     }
     static void swap(int[] arr, int index1, int index2 ){
+        // temp ka matlab hai temporary variable -> ek aisa extra storage jisme hum ek value thodi der ke liye rakh lete hain, taki value overwrite na ho.
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
