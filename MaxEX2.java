ad570514a9984a78fadfe0efea07404452d92cd4
Tanzil <tanzilibrahim5@gmail.com>
2025-08-21 06:35:49 +0530

    update

--- MaxEX2.java
+++ MaxEX2.java
@@ -1,10 +1,17 @@
-public class MaxEx {
+public class MaxEX2 {
     public static void main(String[] args) {
         int[] arr = {1, 3, 23, 9, 18};
         System.out.println(maxRange(arr, 1, 3));
     }
-
+    // work on edge cases here, like array being null
     static int maxRange(int[] arr, int start, int end) { 
+        if (end > start) {
+            return -1;
+        }
+        if (arr == null) {
+            return -1;
+        }
+
         int maxVal = arr[start]; 
         for (int i = start; i <= end; i++){
             if (arr[i] > maxVal){ 
@@ -15,6 +22,10 @@ public class MaxEx {
     }
     // imagine that arr is not empty
     static int max(int[] arr) { // create function for max.
+        // imagine that arr is empty
+        if (arr.length == 0) {
+            return -1;
+        }
         int maxVal = arr[0]; // starting assumption: pehla element hi max.
         for (int i = 1; i < arr.length; i++){
             if (arr[i] > maxVal){ //agar current element max se bada hai.
