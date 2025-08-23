d2b7fc15ed98a08fc1e1813db96f6b619f4e4577
Tanzil <tanzilibrahim5@gmail.com>
2025-08-23 10:40:23 +0530

    update

---
+++ LinearSearch.java
@@ -0,0 +1,29 @@
+public class LinearSearch {
+    
+    // search in the array: return the index if item found
+    // otherwise if item not found return -1
+    static int LinearSearch(int[] arr, int target){
+        // check array is empty or not
+        if(arr.length == 0){
+            return -1;
+        }
+        // run a  for loop
+        for(int index = 0; index < arr.length; index++){
+            //check for element at every index if it is  = target
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+        }
+        // this line will execute if none of the reurn statements above have executed
+        // hence the target not found
+        return -1;
+    }
+        public static void main(String[] args){
+            int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+            int target = 19;
+            int ans = LinearSearch(nums, target);
+            System.out.println(ans);
+        }
+    
+}
