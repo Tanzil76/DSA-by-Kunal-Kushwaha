06efef072a52a412f5116801499bfb077ddd7520
Tanzil <tanzilibrahim5@gmail.com>
2026-01-30 19:05:53 +0530

    update

---
+++ InsrtionSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class InsrtionSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 9, 13, 15, 7};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertion(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
