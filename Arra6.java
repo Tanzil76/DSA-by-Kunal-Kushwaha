a11af98572e2876284c55a27e3599368964f1f41
Tanzil <tanzilibrahim5@gmail.com>
2026-02-11 20:22:57 +0530

    update

--- Arra6.java
+++ Arra6.java
@@ -1,23 +1,11 @@
 import java.util.Scanner;
 
 public class Arra6 {
-     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-         System.out.print("Enter size of an array " + " ");
-        int n = sc.nextInt();
-        System.out.print("Array elements " + " ");
-        int[] arr = new int[n];
-      
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.print("Tell the number " + " ");
-        int x = sc.nextInt();
-        System.out.println("Last Occurence of X: " + LastOccurences(arr, x));
-    }
      static int LastOccurences(int[] arr , int x){
+
         int lastIndex = -1;
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
                 lastIndex = i;
@@ -26,5 +14,21 @@ public class Arra6 {
         
         return lastIndex;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+      
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+        System.out.println("Last Occurence of X: " + LastOccurences(arr, x));
+    }
+
+    
 }
 
