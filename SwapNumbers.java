9cd54816184db34bb5d9f7129ce615b30fb7d312
Tanzil <tanzilibrahim5@gmail.com>
2026-02-15 14:48:27 +0530

    update

---
+++ SwapNumbers.java
@@ -0,0 +1,24 @@
+public class SwapNumbers {
+
+    // a = 9, b = 3
+    static void swap(int a, int b){
+        System.out.println("Original values before swap");
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Values after swap");
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 3;
+        int[] arr = {1, 2, 3, 4, 5};
+        swap(a, b);
+    }
+}
