babfc9e6df2536a5651922084876d5f1a730e551
Tanzil <tanzilibrahim5@gmail.com>
2026-02-25 20:36:49 +0530

    update

--- PrefixSum.java
+++ PrefixSum.java
@@ -11,17 +11,32 @@ public class PrefixSum {
         System.out.println();
     }
 
+    static int[] mekPrefixSumArray(int[] arr){
+        int n = arr.length;
+        int[] pref = new int[n];
+        pref[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i-1] + arr[i];
+        }
+        return pref;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter array size: ");
+        System.out.print("Enter array size: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        System.out.println("Enter " + n + " elements: ");
+        System.out.print("Enter " + n + " elements: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println();
+        System.out.print("Input Array: ");
         printArray(arr);
+
+        int[] pref = mekPrefixSumArray(arr);
+        System.out.print("Prefix Sum: ");
+        printArray(pref);
     }
 }
