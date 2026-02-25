b0f27ce3961337e149ea586e1a992a2f6cc24583
Tanzil <tanzilibrahim5@gmail.com>
2026-02-25 19:23:28 +0530

    update

--- PrefixSum.java
+++ PrefixSum.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class PrefixSum {
-    static void  
+
+    static void printArray(int[] arr){
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println();
+        printArray(arr);
+    }
 }
