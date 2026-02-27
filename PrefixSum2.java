c80520d4b689b4202fbe9a847504952ebf01c004
Tanzil <tanzilibrahim5@gmail.com>
2026-02-27 15:18:13 +0530

    update

--- PrefixSum2.java
+++ PrefixSum2.java
@@ -1,13 +1,41 @@
+// Give an array integers of size n. Answer q queries where you need to print the sum of values in a given range of indices from l to r 
+// (both included). Note: The values of l and r in queries follow 1-based indexing.
+
+import java.util.Scanner;
+
 public class PrefixSum2 {
-    static void printArray(int[] arr){
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+
     static int[] makePrefixSumArray(int[] arr){
-        int n = arr.length;
-        
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i-1];
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+
+        System.out.println("Enter " + n + " elements: ");
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] prefSum = makePrefixSumArray(arr);
+
+        System.out.println("Enter number of queries: ");
+        int q = sc.nextInt();
+
+        while (q-- > 0){
+            System.out.println("Enter range: ");
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+
+            int ans = prefSum[r] - prefSum[l-1];
+
+            System.out.println("Sum " + ans);
+        }
     }
 }
