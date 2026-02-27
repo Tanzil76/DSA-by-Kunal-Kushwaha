2cf1b476e41cc6b7ae56841995929e207e921911
Tanzil <tanzilibrahim5@gmail.com>
2026-02-27 19:22:35 +0530

    update

---
+++ SuffixSum.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SuffixSum {
+
+    static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    static int[] makeSuffixSumArray(int[] arr){
+        int n = arr.length;
+
+        for (int i = n-2; i >= 0; i--) {
+            arr[i] = arr[i] + arr[i+1];
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + "elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Input Array: ");
+        printArray(arr);
+
+        int[] suff = makeSuffixSumArray(arr);
+        System.out.println("Suffix Sum: ");
+        printArray(suff);
+    }
+}
