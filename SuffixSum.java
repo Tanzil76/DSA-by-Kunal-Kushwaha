2a9426b1cab48b68f5297441e261d643d44a6656
Tanzil <tanzilibrahim5@gmail.com>
2026-02-27 19:24:02 +0530

    update

--- SuffixSum.java
+++ SuffixSum.java
@@ -18,19 +18,19 @@ public class SuffixSum {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter array size: ");
+        System.out.print("Enter array size: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        System.out.println("Enter " + n + "elements: ");
+        System.out.print("Enter " + n + " elements: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("Input Array: ");
-        printArray(arr);
+    //    System.out.print("Input Array: ");
+    //    printArray(arr);
 
         int[] suff = makeSuffixSumArray(arr);
-        System.out.println("Suffix Sum: ");
+        System.out.print("Suffix Sum: ");
         printArray(suff);
     }
 }
