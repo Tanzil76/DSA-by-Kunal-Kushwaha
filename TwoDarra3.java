e8d67991fa80f7820102aa820e74eac3d301925a
Tanzil <tanzilibrahim5@gmail.com>
2026-03-30 19:02:16 +0530

    update

--- TwoDarra3.java
+++ TwoDarra3.java
@@ -3,8 +3,19 @@
 import java.util.Scanner;
 
 public class TwoDarra3 {
+    static void add(int[][] a, int r1, int c1, int[][] b, int r2, int c2){
+        if(r1 != r2 || c1 != c2){
+            System.out.println("Wrong Input - Addition not possible");
+            return;
+        }
+
+        int[][] sum = new int[r1][c1];
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Enter number of rows and columns: ");
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        int[][] a = new int[r1][c1];
     }
 }
