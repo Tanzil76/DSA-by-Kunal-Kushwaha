c239976a5ece082a86f6ebb6668043a5226986f4
Tanzil <tanzilibrahim5@gmail.com>
2026-04-01 18:59:29 +0530

    update

--- TwoDarra4.java
+++ TwoDarra4.java
@@ -1,5 +1,14 @@
 // Write a program to display Multiplication of two matrices entered by the user.
 
 public class TwoDarra4 {
-    
+    static void printMatrix(int[][] matrix){
+    }
+    static void multiply(int[][] a, int r1, int c1, int[][] b, int r2, int c2){
+        if(c1 != r2){
+            System.out.println("Multiplication not possbile - wrong dimension: ");
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
 }
