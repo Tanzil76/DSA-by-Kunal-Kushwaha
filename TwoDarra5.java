cc66389548e08ac16f4dd4ff8d87180b93a9163d
Tanzil <tanzilibrahim5@gmail.com>
2026-04-06 12:51:45 +0530

    update

--- TwoDarra5.java
+++ TwoDarra5.java
@@ -1,8 +1,9 @@
-// Write a program to display transpose of matrix by the user.
+  // Write a program to display transpose of matrix by the user.
 
 import java.util.Scanner;
 
 public class TwoDarra5 {
+
     static void printMatrix(int[][] matrix){
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
@@ -11,22 +12,35 @@ public class TwoDarra5 {
             System.out.println();
         }
     }
-}
-public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter number of rows and columns of matrix : ");
-    int r1 = sc.nextInt();
-    int c1 = sc.nextInt();
-    int[][] matrix = new int[r][c];
-    int totalElements = r * c;
-    System.out.println("Enter " + totalElements + " values: ");
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            matrix[i][j] = sc.nextInt();
+    static int[][] findTranspose(int[][] matrix, int r, int c){
+        int[][] ans = new int[c][r];
+
+        for(int i = 0; i < c; i++){
+            for(int j = 0; j < r; j++){
+                ans[i][j] = matrix[j][i];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows and columns of matrix : ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        int totalElements = r * c;
+        System.out.println("Enter " + totalElements + " values: ");
+            for(int i = 0; i < r; i++){
+                for(int j = 0; j < c; j++){
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println("Input Matrix: ");
+            printMatrix(matrix);
+
+            System.out.println("Transpose of matrix: ");
+            int[][] ans = findTranspose(matrix, r, c);
+            printMatrix(ans);
         }
     }
-    System.out.println("Input Matrix: ");
-    priintMatrix(matrix);
 
-    System.out.println("Transpose of matrix: ");
-}
