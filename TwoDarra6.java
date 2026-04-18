f5b0e35c4e921d4f13a1d80b4bda1944ac663d15
Tanzil <tanzilibrahim5@gmail.com>
2026-04-18 14:34:43 +0530

    update

---
+++ TwoDarra6.java
@@ -0,0 +1,10 @@
+
+public class TwoDarra6 {
+    static void printMatrix(int[][] matrix){
+        static void transposeInPlace(int[][] matrix, int r, int c){
+
+            static void rotate(int[][] matrix, int n){
+            }
+        }
+    }
+}
