6195ca530e0d228d5c9fbd2b38418439ff05a719
Tanzil <tanzilibrahim5@gmail.com>
2026-04-18 18:01:30 +0530

    update

--- TwoDarra6.java
+++ TwoDarra6.java
@@ -1,10 +1,40 @@
 
-public class TwoDarra6 {
-    static void printMatrix(int[][] matrix){
-        static void transposeInPlace(int[][] matrix, int r, int c){
+  // Write a program to display transpose of matrix by the user.
 
-            static void rotate(int[][] matrix, int n){
+import java.util.Scanner;
+
+public class TwoDarra5 {
+
+    static void printMatrix(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
             }
+            System.out.println();
         }
     }
-}
+    static int[][] rotate(int[][] matrix, int n){
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows and columns of matrix : ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        int totalElements = r * c;
+        System.out.println("Enter " + totalElements + " values: ");
+            for(int i = 0; i < r; i++){
+                for(int j = 0; j < c; j++){
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println("Input Matrix: ");
+            printMatrix(matrix);
+
+            System.out.println("Transpose of matrix: ");
+            int[][] ans = findTranspose(matrix, r, c);
+            printMatrix(ans);
+        }
+    }
+
