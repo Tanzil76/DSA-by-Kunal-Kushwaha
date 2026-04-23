e297895642d612925479e5968cf311173d5d8f72
Tanzil <tanzilibrahim5@gmail.com>
2026-04-23 15:51:42 +0530

    update

---
+++ ArraList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class ArraList {
+    public static void main(String[] args) {
+        // wrapper classes
+//    Integer i = Integer.valueOf(4);
+//    System.out.println(i);
+//    Float f = Float.valueOf(4.5);
+//    System.out.println(f);
+
+    ArrayList<Integer> l1 = new ArrayList<>();
+
+    // add new element
+    l1.add(5);
+    l1.add(6);
+    l1.add(7);
+    l1.add(8);
+
+    // get an element at index i
+    System.out.println(l1.get(1)); // 6
+
+    // print with for loop
+    for (int i = 0; i < l1.size(); i++) {
+        System.out.println(l1.get(i)); // 5, 6, 7, 8
+    }
+    
+    }
+}
--- TwoDarra6.java
+++ TwoDarra6.java
@@ -1,40 +1,51 @@
 
-  // Write a program to display transpose of matrix by the user.
+  // Write a program to 
 
 import java.util.Scanner;
 
-public class TwoDarra5 {
+public class TwoDarra6 {
 
     static void printMatrix(int[][] matrix){
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
     }
-    static int[][] rotate(int[][] matrix, int n){
-        
-    }
+        static void transposeInPlace(int[][] matrix, int r, int c){
+            for (int i = 0; i < c; i++) {
+                for (int j = 0; j < r; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+
+            static void rotate(int[][] matrix, int n){
+                // transpose
+                // reverse each row of transposed matrix
+                transposeInPlace(matrix, n, n);
+            }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rows and columns of matrix : ");
+        System.out.println("Enter number of rows and columns: ");
         int r = sc.nextInt();
         int c = sc.nextInt();
         int[][] matrix = new int[r][c];
         int totalElements = r * c;
-        System.out.println("Enter " + totalElements + " values: ");
-            for(int i = 0; i < r; i++){
-                for(int j = 0; j < c; j++){
-                    matrix[i][j] = sc.nextInt();
-                }
+        System.out.println("Enter " + totalElements + " values : ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
             }
-            System.out.println("Input Matrix: ");
-            printMatrix(matrix);
-
-            System.out.println("Transpose of matrix: ");
-            int[][] ans = findTranspose(matrix, r, c);
-            printMatrix(ans);
         }
+        System.out.println("Input Matrix: ");
+        printMatrix(matrix, r);
+
+        System.out.println("Rotation of matrix: ");
     }
+}
+
 
