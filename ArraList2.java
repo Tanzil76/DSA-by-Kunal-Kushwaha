f16efdd73684ac45a4ba344da3ca7970ffa02e94
Tanzil <tanzilibrahim5@gmail.com>
2026-04-24 15:26:38 +0530

    update

---
+++ ArraList2.java
@@ -0,0 +1,25 @@
+// write a program to sort an ArrayList of Strings in descending order.
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArraList2 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> list = new ArrayList<>();
+        
+        list.add("ArrayList");
+        list.add("is");
+        list.add("a");
+        list.add("resizable");
+        list.add("array");
+
+        System.out.println("Original List: " + list);
+        
+        // Sort in descendinfg order
+        Collections.sort(list, Collections.reverseOrder());
+
+        System.out.println("Descending Order List: " + list);
+    }
+}
