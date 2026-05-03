f9cacabd8a5aa633b3841e579a15fd914c43e441
Tanzil <tanzilibrahim5@gmail.com>
2026-05-03 20:13:45 +0530

    update

---
+++ Recursion2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Recursion2 {
+
+    static void printDecreasing(int n){ // 5 4 3 2 1
+
+        // base case
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        // Self work
+        System.out.println(n);
+
+        // recursive work
+        printDecreasing(n-1);
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printDecreasing(n);
+    }
+}
