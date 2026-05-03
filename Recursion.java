e376b1f4ad8e1ae34bcab2673782604182aa8e9c
Tanzil <tanzilibrahim5@gmail.com>
2026-05-03 16:20:40 +0530

    update

--- Recursion.java
+++ Recursion.java
@@ -6,17 +6,16 @@ public class Recursion {
 
     static void printIncreasing(int n){
         if(n == 1){
-            System.out.println(n);
+            System.out.println(n);         // -> Base Case
             return;
         }
 
-        printIncreasing(n-1); // 1, 2, .... n-1
-        System.out.println(n);
+        printIncreasing(n-1); // 1, 2, .... n-1    -> Smaller subproblem
+        System.out.println(n);              // Self Work
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
         printIncreasing(n);
     }
 }
