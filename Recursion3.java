c28b2cb160afea64da8d44b7be633366e55383dd
Tanzil <tanzilibrahim5@gmail.com>
2026-05-06 18:24:59 +0530

    update

---
+++ Recursion3.java
@@ -0,0 +1,25 @@
+// Find the value of factorial 5.
+
+import java.util.Scanner;
+
+public class Recursion3{
+
+    static int factorial(int n){  // n = 5
+
+        // Base case
+        if(n == 0){
+            return 1;
+        }
+
+        //  Smaller problem - Recursive work
+        int smallAns = factorial(n - 1);  // factorial(4) = 24
+
+        // Big problem - self work
+        int ans = n * smallAns; // n * factorial(n-1)
+        return ans;
+
+    }
+    public static void main(String[] args){
+        System.out.println(factorial(5));
+    }
+}
