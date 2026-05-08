3f16e2e4099737a825f7c7c6ce71a5504102bf1c
Tanzil <tanzilibrahim5@gmail.com>
2026-05-08 19:36:20 +0530

    update

--- Recursion5.java
+++ Recursion5.java
@@ -1,7 +1,21 @@
 // Given an integer, find out the sum of its digits using recursion.
 
 public class Recursion5 {
+
+    static int sod(int n){
+        // Base Case
+        if(n >= 0 && n <= 9){
+            return n;
+        }
+
+        // recursive work - small ans
+        int smallAns = sod(n/10);
+
+        // self work
+        int ans = smallAns + n % 10;
+        return ans;
+    }
     public static void main(String[] args){
-        
+        System.out.println(sod(1234));
     }
 }
